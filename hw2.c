8ba4b6dd0827f6edf3a76a7e9b6300627b033758
hyewon319 <163431761+hyewon319@users.noreply.github.com>
2024-09-18 21:14:32 +0900

    Add files via upload

---
+++ hw2.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	float km, mile;
+	printf("Please enter kilometers: ");
+	scanf("%f", &km);
+
+	mile = km / 1.609;
+	printf("%.1f km is equal to %.1f miles \n", km, mile);
+	return 0;
+}
